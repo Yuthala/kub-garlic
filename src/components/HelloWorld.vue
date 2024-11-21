d41ebdf54a157934014496399815f3decb0c67e9
Yuthala <dinapolousova@gmail.com>
2024-11-21 15:23:14 +0300

    favicon, title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <button 
+  class="btn btn-primary" 
+  data-bs-target="#collapseTarget" 
+  data-bs-toggle="collapse">
+  Bootstrap collapse
+</button>
+<div class="collapse py-2" id="collapseTarget">
+  This is the toggle-able content!
+</div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the

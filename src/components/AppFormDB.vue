6f6d4a92f18216906911a744fc6920968ebf83cd
Yuthala <dinapolousova@gmail.com>
2024-12-07 05:31:28 +0300

    first input DB

--- src/components/AppFormDB.vue
+++ src/components/AppFormDB.vue
@@ -1,14 +1,14 @@
 <template>
 	<div class="container">
 		<form class="card" @submit.prevent="createPerson">
-			<h2>Работа с базой данных</h2>
+			<h2>Напишите какой сорт вас заинтересовал</h2>
 
 			<div class="form-control">
-				<label for="name">Введите имя</label>
+				<label for="name">Введите сорт</label>
 				<input class="form-control" type="text" id="name" v-model.trim="name">
 			</div>
 
-			<button type="button" class="btn btn-success" :disabled="name.length === 0" >Создать человека</button>
+			<button class="btn btn-success" :disabled="name.length === 0" >Отправить запрос</button>
 		</form>
 	</div>
 </template>
@@ -21,8 +21,23 @@ export default {
 		}
 	},
 	methods: {
-		createPerson() {
+		async createPerson() {
+			//https://vue-kub-garlic-default-rtdb.firebaseio.com/sort.json
 			//this.name
+
+			const response = await fetch('https://vue-kub-garlic-default-rtdb.firebaseio.com/sort.json', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					firstName: this.name
+				})
+			})
+
+			const firebaseData = await response.json()
+			console.log(firebaseData)
+			this.name = ''
 		}
 	}
 }
